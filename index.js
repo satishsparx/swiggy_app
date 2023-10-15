1a0a894046dd52180d850ce08c692e2d352c7950
Satish Kumar <satishkumar.edu@gmail.com>
2023-10-15 15:04:47 +0530

    React context & redux setup

--- index.js
+++ index.js
@@ -8,17 +8,21 @@ import {createBrowserRouter,  RouterProvider, Outlet} from "react-router-dom"
 import './src/App.css'
 import RestaurantMenu from './src/components/RestaurantMenu'
 import UserContext from './src/utils/UserContext'
+import { Provider } from 'react-redux'
+import appStore from './src/utils/store/appStore'
 
 const About = lazy(() => import("./src/components/About"))
 
 const App = () => {
     return (
-        <UserContext.Provider value={{loggedInUser: "Swiggy user", country: "India"}}>
-            <div id="app">
-                <Header />
-                <Outlet />
-            </div>
-        </UserContext.Provider>
+        <Provider store={appStore}>
+            <UserContext.Provider value={{loggedInUser: "Swiggy user", country: "India"}}>
+                <div id="app">
+                    <Header />
+                    <Outlet />
+                </div>
+            </UserContext.Provider>
+        </Provider>
     )
 }
 
